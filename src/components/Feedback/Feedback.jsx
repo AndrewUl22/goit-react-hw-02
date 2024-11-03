fbc114bc214c125e85cbb776ee960d5e7aaa1694
Andrew <anulian22@gmail.com>
2024-11-03 14:08:36 +0200

    R2hw

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,14 +1,19 @@
 import css from './Feedback.module.css';
 import React from 'react'
 
-export default function Feedback({ valueFeedBack, totalFeedback, valueFeedBack.positive }) {
+
+const Feedback = ({ valueFeedBack, totalFeedback, positiveFeedback }) => {
+
   return (
     <ul className={css.feedback_stats_wrapper}>
       <li className={css.feedback_stats}>Good: <span>{valueFeedBack.good }</span></li>
       <li className={css.feedback_stats}>Neutral: <span>{valueFeedBack.neutral }</span></li>
       <li className={css.feedback_stats}>Bad: <span>{valueFeedBack.bad}</span></li>
       <li className={css.feedback_stats}>Total: <span>{ totalFeedback }</span></li>
-      <li className={css.feedback_stats}>Positive: <span>{valueFeedBack.positive }% </span></li>
+      <li className={css.feedback_stats}>Positive: <span>{positiveFeedback  }% </span></li>
     </ul>
   )
-}
+};
+
+export default Feedback
+
