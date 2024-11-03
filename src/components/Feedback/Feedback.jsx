c270a56361284d285edfee213522d7368972c6c3
Andrew <anulian22@gmail.com>
2024-11-03 13:48:14 +0200

    R2hw

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,7 @@
-import css from './Feedback.module.css'
+import css from './Feedback.module.css';
+import React from 'react'
 
-export default function Feedback({ valueFeedBack, totalFeedback }) {
+export default function Feedback({ valueFeedBack, totalFeedback, valueFeedBack.positive }) {
   return (
     <ul className={css.feedback_stats_wrapper}>
       <li className={css.feedback_stats}>Good: <span>{valueFeedBack.good }</span></li>
